6c6382c19db2daae8260dea096442a8d625d3395
Uladzislau Korabau <vladkorobov.work@gmail.com>
2025-05-15 17:56:53 +0200

    add: krok 1

---
+++ kolokwium/src/Clock.java
@@ -0,0 +1,38 @@
+import java.time.LocalTime;
+
+public abstract class Clock {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Clock() {
+
+    }
+
+    public void setCurrentTime() {
+        this.hours = LocalTime.now().getHour();
+        this.minutes = LocalTime.now().getMinute();
+        this.seconds = LocalTime.now().getSecond();
+    }
+
+    public void setTime(int hours, int minutes, int seconds) {
+        if (hours >= 24 || hours < 0) {
+            throw new IllegalArgumentException("Hours can't be greater or equal 24 and less than 0");
+        }
+        if (minutes >= 60 || minutes < 0) {
+            throw new IllegalArgumentException("Minutes can't be greater or equal 60 and less than 0");
+        }
+        if (seconds >= 60 || seconds < 0) {
+            throw new IllegalArgumentException("Seconds can't be greater or equal 60 and less than 0");
+        }
+
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public String toString() {
+        return  this.hours + ":" + this.minutes + ":" + this.seconds;
+    }
+}
